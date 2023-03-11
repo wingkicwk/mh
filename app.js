95474882e63388a798fc787d26218b4ccd0516d1
wingkicwk <mnicwk@gmail.com>
2023-03-11 17:07:11 +0000

    simple post request with time needed calculation

--- app.js
+++ app.js
@@ -2,10 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(req, res) {
+// Middleware to parse JSON request body
+app.use(express.json());
+
+app.get('/', function (req, res) {
     res.send("hello world!");
 })
+// Define API endpoint
+app.post('/api/savings', async (req, res) => {
+    try {
 
-app.listen(3000, function(){
-    console.log("example app listening on port 3000");
-})
+        // Extract user input from request body
+        const { target, monthlyIncome, mothlySpending, curSaving } = req.body;
+
+        //calculae time needed to save up
+        const mothlyNetSaving = monthlyIncome - mothlySpending;
+        const remaining = target - curSaving;
+        const timeNeeded = (target - curSaving) / mothlyNetSaving;
+
+        // Return response as JSON
+        res.status(200).json({
+            timeNeeded,
+            mothlyNetSaving,
+            remaining
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
