fb87e1ba315eb2ce7c48dfd38fa6f20dc41b35ca
wingkicwk <mnicwk@gmail.com>
2023-03-13 22:25:49 +0000

    formatting

--- frontend/savings-calculator.js
+++ frontend/savings-calculator.js
@@ -1,4 +1,3 @@
-
 const baseUrl = 'http://localhost:8383'
 
 const submitBtn = document.getElementById('submit')
@@ -12,37 +11,36 @@ const monthlyIncomeError = document.getElementById('monthlyIncomeError')
 const monthlySpendingError = document.getElementById('monthlySpendingError')
 const curSavingError = document.getElementById('curSavingError')
 
-const result = document.getElementById('result');
+const result = document.getElementById('result')
 
-submitBtn.addEventListener('click', (event) => {
-    event.preventDefault(); // to prevent default action so can define own event behavior
+submitBtn.addEventListener('click', event => {
+  event.preventDefault() // to prevent default action so can define own event behavior
 
-    clearResultAndError()
+  clearResultAndError()
 
-    // only submit if all inputs are valid
-    if (isAllInputsValid()) {
-        const data = {
-            target: targetInput.value,
-            monthlyIncome: monthlyIncomeInput.value,
-            monthlySpending: monthlySpendingInput.value,
-            curSaving: curSavingInput.value
-        };
-        submitData(data);
+  // only submit if all inputs are valid
+  if (isAllInputsValid()) {
+    const data = {
+      target: targetInput.value,
+      monthlyIncome: monthlyIncomeInput.value,
+      monthlySpending: monthlySpendingInput.value,
+      curSaving: curSavingInput.value
     }
-});
+    submitData(data)
+  }
+})
 
-
-function submitData(data) {
-    fetch(baseUrl + "/api/savings", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+function submitData (data) {
+  fetch(baseUrl + '/api/savings', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      result.textContent = `You will need ${data.timeNeeded} more month(s) to save up to your target of ${data.target}. Remaining amount to save up is $ ${data.remaining}. `
     })
-        .then(response => response.json())
-        .then(data => {
-            result.textContent = `You will need ${data.timeNeeded} more month(s) to save up to your target of ${data.target}. Remaining amount to save up is $ ${data.remaining}. `;
-        })
-        .catch(error => console.error('Error:', error));
+    .catch(error => console.error('Error:', error))
 }
--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.use(express.static('frontend'))
 
 // Middleware to parse JSON request body
-app.use(express.json());
+app.use(express.json())
 
 // Define API endpoint
 app.post('/api/savings', async (req, res) => {
-    try {
-        // Extract user input from request body
-        const { target, monthlyIncome, monthlySpending, curSaving } = req.body;
+  try {
+    // Extract user input from request body
+    const { target, monthlyIncome, monthlySpending, curSaving } = req.body
 
-        //calculae time needed to save up
-        const monthlyNetSaving = monthlyIncome - monthlySpending;
-        const remaining = target - curSaving;
-        const timeNeeded = remaining / monthlyNetSaving;
+    //calculae time needed to save up
+    const monthlyNetSaving = monthlyIncome - monthlySpending
+    const remaining = target - curSaving
+    const timeNeeded = remaining / monthlyNetSaving
 
-        // Return response as JSON
-        res.status(200).json({
-            timeNeeded,
-            target,
-            remaining
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error');
-    }
-});
+    // Return response as JSON
+    res.status(200).json({
+      timeNeeded,
+      target,
+      remaining
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server error')
+  }
+})
 
 // Start the server
-const port = process.env.PORT || 8383;
+const port = process.env.PORT || 8383
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
