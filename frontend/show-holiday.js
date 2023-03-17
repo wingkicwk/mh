2f7efdee10e22e3713d35e9db05b15391d520de2
yiyuan geng <yiyuangeng@yiyuandeMacBook-Pro.local>
2023-03-17 15:50:36 +0000

    extract the js file from html

---
+++ frontend/show-holiday.js
@@ -0,0 +1,21 @@
+
+const result = document.getElementById('holidayResult');
+const baseUrl = 'http://127.0.0.1:8383';
+document.addEventListener("DOMContentLoaded", function (event) {
+    const holidayUrl = `${baseUrl}/api/showCalendar`;
+    fetch(holidayUrl, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+    })
+    .then(response => response.json())
+    .then(data => {
+    if(data[0].date > 0){
+        holidayResult.textContent = `Till next holiday ${data[0].summary} is ${data[0].date} days left`;
+    }else{
+        holidayResult.textContent = `Today is ${data[0].summary}`;
+    }
+    })
+    .catch(error => console.error('Error:', error));
+});
