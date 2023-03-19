32ca8aad5f8fb101a081806cd707c2db5f88368d
yiyuan geng <yiyuangeng@yiyuandeMacBook-Pro.local>
2023-03-19 20:46:42 +0000

    try 2 to merge

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('frontend'))
 
 // Middleware to parse JSON request body
 app.use(express.json())
-const API_KEY = process.env.Google_API_KEY
+const API_KEY = process.env.GOOGLE_API_KEY
 
 // Define API endpoint
 app.post('/api/savings', async (req, res) => {
@@ -49,13 +49,11 @@ async function getHoliday () {
   const BASE_CALENDAR_URL = 'https://www.googleapis.com/calendar/v3/calendars'
   const BASE_CALENDAR_ID_FOR_PUBLIC_HOLIDAY =
       'holiday@group.v.calendar.google.com' // Calendar Id. This is public but apparently not documented anywhere officialy.
-  // const API_KEY = 'AIzaSyBg0IiUjXv79hXM_V5aYxKAd6erKPPYvCU'
-
   const CALENDAR_REGION = 'en.irish' // This variable refers to region whose holidays do we need to fetch
 
   const url = `${BASE_CALENDAR_URL}/${CALENDAR_REGION}%23${BASE_CALENDAR_ID_FOR_PUBLIC_HOLIDAY}/events?key=${API_KEY}`
 
-  // console.log(url)
+  console.log(url)
   const response = await fetch(url)
   const data = await response.json()
   const holidays = data.items
@@ -74,12 +72,12 @@ async function filterCurrentYearHoliday () {
     const time = singleItem.start.date
     const timeToCompare = time.split('-')[0]
 
-    if (timeToCompare == currentYear) { // add holidays only within current year
+    if (timeToCompare === currentYear) { // add holidays only within current year
       holidaysAndDate.push({ // if the holidays are current years', add the name and time into the result
         summary: summay,
         time
       })
-      if (summay == "New Year's Eve") {
+      if (summay === "New Year's Eve") {
         break
       }
     }
@@ -102,7 +100,7 @@ async function calculateDateToNextHoliday () {
     const singleHolidayTimeParse = Date.parse(singleHolidayTime)
     const preHolidayParse = Date.parse(holidays[i - 1].time)
 
-    if (singleHolidayTimeParse == currentDateParse || (singleHolidayTimeParse > currentDateParse && preHolidayParse < currentDateParse)) {
+    if (singleHolidayTimeParse === currentDateParse || (singleHolidayTimeParse > currentDateParse && preHolidayParse < currentDateParse)) {
       dateNumber = (singleHolidayTimeParse - currentDateParse) / (1000 * 3600 * 24)
       res.push({
         summary: singleHoliday.summary,
